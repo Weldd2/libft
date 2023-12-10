a1bc5875f2532a2fdae58647bbb84154d03a28ea
Your Name <you@example.com>
2023-12-10 18:51:53 +0100

    Fix ft_itoa function to correctly allocate memory for the result string

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,13 @@
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 15:46:38 by amura             #+#    #+#             */
-/*   Updated: 2023/12/10 18:47:15 by antoinemura      ###   ########.fr       */
+/*   Updated: 2023/12/10 18:51:27 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int	count_digit(long n)
 {
@@ -26,7 +28,7 @@ int	count_digit(long n)
 		n /= 10;
 		i++;
 	}
-	return (i);
+	return (i + 1);
 }
 
 char	*ft_itoa(int n)
@@ -38,6 +40,7 @@ char	*ft_itoa(int n)
 	n_cpy = (long)n;
 	size = count_digit(n) + (n < 0);
 	r = (char *)malloc(sizeof(char) * (size + 1));
+	printf("%d\n", size + 1);
 	if (!r)
 		return (NULL);
 	if (n == 0)
@@ -54,4 +57,10 @@ char	*ft_itoa(int n)
 	}
 	r[count_digit(n) + (n < 0)] = '\0';
 	return (r);
+}
+
+
+int	main(void)
+{
+	printf("%s", ft_itoa(3));
 }
