aee8f1e43a15b9da6a97a47c6e5c11ee7bd5aef1
Your Name <you@example.com>
2024-05-25 16:04:57 +0200

    abs

--- math/ft_abs.c
+++ math/ft_abs.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   math.h                                             :+:      :+:    :+:   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/17 20:18:06 by antoinemura       #+#    #+#             */
-/*   Updated: 2024/05/25 16:04:49 by antoinemura      ###   ########.fr       */
+/*   Created: 2024/05/25 16:04:20 by antoinemura       #+#    #+#             */
+/*   Updated: 2024/05/25 16:04:22 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_H
-# define MATH_H
-
-int	ft_pow(int nb, int exposant);
-int	ft_abs(int val);
-
-#endif
+int	ft_abs(int val)
+{
+	if (val < 0)
+		val = -val;
+	return (val);
+}
